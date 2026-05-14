bab3b7138be634775c85dbcbc1d9a3a8544292df
KirillBukato <kirill.bukato06@gmail.com>
2026-05-14 22:50:42 +0300

    cleanup

--- voronoi_planner.h
+++ voronoi_planner.h
@@ -11,21 +11,21 @@
 namespace sdc {
 
     inline geom::Polyline PlanPath(const std::vector<geom::Polygon>& polygons, const geom::Point& start, const geom::Point& finish) {
-        const seg_diag::VoronoiGraph vg = seg_diag::BuildVoronoiGraph(polygons, start, finish);
-        if (vg.vertices.empty()) {
+        const seg_diag::VoronoiGraph voronoi_diagram = seg_diag::BuildVoronoiGraph(polygons, start, finish);
+        if (voronoi_diagram.vertices.empty()) {
             return {start, finish};
         }
 
-        const size_t src = geom::NearestPoint(vg.vertices, start);
-        const size_t dst = geom::NearestPoint(vg.vertices, finish);
+        const size_t src = geom::NearestPoint(start, voronoi_diagram.vertices);
+        const size_t dst = geom::NearestPoint(finish, voronoi_diagram.vertices);
 
-        const auto indices = dijkstra::FindShortestPath(vg.adjacency, src, dst);
+        const auto indices = dijkstra::FindShortestPath(voronoi_diagram.adjacency, src, dst);
 
         geom::Polyline path;
         path.push_back(start);
         for (size_t i : indices) {
-            if (i < vg.vertices.size()) {
-                path.push_back({vg.vertices[i].x, vg.vertices[i].y});
+            if (i < voronoi_diagram.vertices.size()) {
+                path.push_back({voronoi_diagram.vertices[i].x, voronoi_diagram.vertices[i].y});
             }
         }
         path.push_back(finish);
